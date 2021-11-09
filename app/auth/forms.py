b3fd9e269a3c1f0e32e7eb5087ce8b5c1f225469
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-11-09 21:49:41 +0300

    Updated the forms.py file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,11 +7,11 @@ from wtforms import ValidationError
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
-    username = StringField('Enter your username',validators = [Required()])
+    email = StringField('Enter Email',validators=[Required(),Email()])
+    username = StringField('Enter Username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    submit = SubmitField('Sign Up')
+    password_confirm = PasswordField('Confirm Password',validators = [Required()])
+    submit = SubmitField('Create Account')
     
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
